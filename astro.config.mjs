70eb5399375153af45d2a4039b60bc03117e1dae
haroldmud <haroldmudosa@gmail.com>
2025-12-03 16:48:50 +0200

    feat: setting up i18n

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+// @ts-ignore
+import astroI18next from "astro-i18next";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [astroI18next()]
+});
