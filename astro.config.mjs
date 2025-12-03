6fb8b30fc9e2ef82e5416f50a34a2d3416cf5b7a
haroldmud <haroldmudosa@gmail.com>
2025-12-03 17:25:22 +0200

    feat: implementing the contentn

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,8 @@ import { defineConfig } from 'astro/config';
 // @ts-ignore
 import astroI18next from "astro-i18next";
 
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [astroI18next()]
+  integrations: [astroI18next()],
 });
