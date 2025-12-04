2d9762fbfb558eff509b8fdf60def90342797574
haroldmud <haroldmudosa@gmail.com>
2025-12-04 08:56:26 +0200

    feat: initializing a new proj]

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,5 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-// @ts-ignore
-import astroI18next from "astro-i18next";
-
 
 // https://astro.build/config
-export default defineConfig({
-  integrations: [astroI18next()],
-});
+export default defineConfig({});
