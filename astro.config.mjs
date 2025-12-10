4c68921d49c35450076bb316cdad2312e85daa2b
haroldmud <haroldmudosa@gmail.com>
2025-12-10 18:26:22 +0200

    feat: implement some changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from "@tailwindcss/vite";
 
+
+// https://astro.build/config
 export default defineConfig({
   i18n: {
     locales: ["en", "de"],
